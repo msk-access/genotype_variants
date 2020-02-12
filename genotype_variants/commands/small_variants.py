bace711e3911eb22c4853a734cded2aeeeca05a2
Ronak Shah <rons.shah@gmail.com>
2020-02-12 11:20:31 -0500

    Update small_variants.py
    Remove unused variable

--- genotype_variants/commands/small_variants.py
+++ genotype_variants/commands/small_variants.py
@@ -472,7 +472,7 @@ def merge(
     logger.info("Elapsed time: %.1f [min]" % ((t1_stop - t1_start) / 60))
     logger.info("CPU process time: %.1f [min]" % ((t2_stop - t2_start) / 60))
     logger.info("--------------------------------------------------")
-    return out_duplex_simplex_maf
+    return filename
 
 
 def write_csv(file_name, data_frame):
