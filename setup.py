b0c0b86d30292d57aa9883f7c4737db8cf6d8b74
buehlere <buehlere@mskcc.org>
2023-12-20 11:42:31 -0500

    fixing reqs and setup
    :heavy_check_mark: locking numpy version as newer versions clash with currently locked pandas version
    :heavy_check_mark: fixing some formatting in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'click_log>=version=0.3.2', 'pandas>=1.0.0', 'xlrd>=1.2.0']
+requirements = ['Click>=7.0', 'click_log>=0.3.2', 'pandas>=1.0.0', 'xlrd>=1.2.0']
 
 setup_requirements = [ ]
 
